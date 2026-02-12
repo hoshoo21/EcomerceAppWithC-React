d7361567e8486037da8629689f1d2bbdeac069ea
hoshoo21 <c_gawera@yahoo.com>
2026-02-12 12:42:44 -0800

    initial comimit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { 
+            
+        }
+        public DbSet<Infrastructure.Entities.Product> Products { get; set; }
+
+
+    }
+}
---
+++ Skinet/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Infrastructure.Entities;
+
+namespace SkinetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private  StoreContext _storecontext;
+       
+        public  ProductsController(StoreContext context)
+        {
+            _storecontext = context;
+        }
+        
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products =await _storecontext.Products.ToListAsync();
+            return Ok(products);   
+        }
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _storecontext.Products.FindAsync(id);
+        }
+
+    }
+}
